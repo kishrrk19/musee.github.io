1a2747c050cadd2f76f950663cfe7e526a3f03e2
Deblak <deblak@mailo.com>
2023-12-21 18:52:50 +0100

    creation liste de filtre javascript

--- scripts/home.js
+++ scripts/home.js
@@ -2,10 +2,36 @@ async function fetchMuseumList () {
 
     const response = await fetch("https://data.culturecommunication.gouv.fr/api/explore/v2.1/catalog/datasets/musees-de-france-base-museofile/records?limit=30")
     const json = await response.json();
-
+ 
+    //appel des resultats de l'API
     const firstMuseumName = json.results[0].nomoff;
     console.log(firstMuseumName);
+
+   //creation tableau pour filtres de themes
+   let globalThemesSet = new Set;
+   let eachMuseumThemesTab = [];
+    //tableau pour stocker les themes
+    json.results.map((museum)=>{
+        eachMuseumThemesTab = museum.dompal;
+        for (let i = 0; i < eachMuseumThemesTab.length; i++){
+            globalThemesSet.add(eachMuseumThemesTab[i]);
+        }
+    });
+    console.log(globalThemesSet[0])
+/*
+     //Boucle qui affiche les themes du musee
+     for(let  i = 0; i < globalThemesSet.length; i++){
+        const theme = globalThemesSet[i];
+       
+        let filterMuseum = 
+        `
+        <option value="">--Faites votre choix--</option>
+        <option value="${theme}">${theme}</option>
+        `
+        filter.innerHTML += filterMuseum;
+    }      */
     
+    //Boucle pour afficher les musees dans un article
     for(let  i = 0; i < json.results.length; i++){
         const museum = json.results[i];
 
@@ -25,8 +51,10 @@ async function fetchMuseumList () {
                 <a href="http://${visit}"><button class="visit"> Visiter le site</button></a> 
             </article>
             <hr/>`
-        targetName.innerHTML += infoMuseum;
-    }       
+            targetMuseum.innerHTML += infoMuseum;
+    }  
+    
+    
 }
    
     window.addEventListener("load", (event) => {
@@ -34,5 +62,6 @@ async function fetchMuseumList () {
         console.log("Ici, load event listener.");
     });
 
-const targetName = document.getElementById("targetName");
+const targetMuseum = document.getElementById("targetMuseum");
+const filter = document.getElementById("filter");
 
